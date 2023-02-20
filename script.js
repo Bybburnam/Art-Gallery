1f423981ce5015e07403cda14ab8d4f374b83173
LukeMcK <lukemck110@gmail.com>
2023-02-20 16:23:02 -0500

    art pieces now display to DOM with title and artist name

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let artArray = [];
+let body = document.querySelector("body");
+
 function getFetch() {
   const url = `https://collectionapi.metmuseum.org/public/collection/v1/objects`;
 
@@ -12,17 +15,21 @@ function getFetch() {
         obj = obj.then((res) => res.json());
         obj = obj
           .then((data) => {
-            console.log(data);
             if (
-              data.primaryImageSmall != "" &&
-              data.artistDisplayName != "" &&
-              data.title != ""
+              data.title &&
+              data.artistDisplayName &&
+              data.primaryImageSmall
             ) {
-              let img = document.createElement("img");
-              let body = document.querySelector("body");
-              img.src = data.primaryImageSmall;
-              body.appendChild(img);
+              console.log(data);
+              artArray.push(
+                new ArtPiece(
+                  data.title,
+                  data.artistDisplayName,
+                  data.primaryImageSmall
+                )
+              );
             }
+            displayArt(artArray);
           })
           .catch((err) => {
             console.log(`error ${err}`);
@@ -35,3 +42,49 @@ function getFetch() {
 }
 
 getFetch();
+
+class ArtPiece {
+  #title;
+  #artist;
+  #art;
+  constructor(title, artist, art) {
+    this.#title = title;
+    this.#artist = artist;
+    this.#art = art;
+  }
+
+  getTitle() {
+    return this.#title;
+  }
+
+  getArtist() {
+    return this.#artist;
+  }
+
+  getArt() {
+    return this.#art;
+  }
+}
+
+function displayArt(arr) {
+  arr.forEach((artPiece) => {
+    let pieceCard = createCard(artPiece);
+    body.appendChild(pieceCard);
+  });
+}
+
+function createCard(artPiece) {
+  let span = document.createElement("section");
+  let img = document.createElement("img");
+  img.src = artPiece.getArt();
+  img.classList.add("artPiece");
+  let pieceTitle = document.createElement("h3");
+  pieceTitle.textContent = artPiece.getTitle();
+  let artistName = document.createElement("p");
+  artistName.textContent = artPiece.getArtist();
+  span.appendChild(img);
+  span.appendChild(pieceTitle);
+  span.appendChild(artistName);
+  span.classList.add("card");
+  return span;
+}
